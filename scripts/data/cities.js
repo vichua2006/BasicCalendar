6ae4fa2b2bdd69b6b1634e4d0e15b0a3e3af4514
Victor Huang <103213638+vichua2006@users.noreply.github.com>
2024-06-26 16:16:20 -0300

    implemented search bar

---
+++ scripts/data/cities.js
@@ -0,0 +1,26 @@
+export var cities = [
+
+    "Las Vegas, NV",
+    "Phoenix, AZ",
+    "Toronto, ON",
+    "Scottsdale, AZ",
+    "Charlotte, NC",
+    "Pittsburgh, PA",
+    "Tempe, AZ",
+    "Henderson, NV",
+    "Mesa, AZ",
+    "Montréal, QC",
+    "Chandler, AZ",
+    "Gilbert, AZ",
+    "Cleveland, OH",
+    "Madison, WI",
+    "Glendale, AZ",
+    "Edinburgh, EDH",
+    "Mississauga, ON",
+    "Peoria, AZ",
+    "Markham, ON",
+    "North Las Vegas, NV",
+    "Fredericton, NB",
+
+];
+
