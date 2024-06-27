12f09bffdca4dfbb69739e368b1d5b8ebe9f185f
RohanKatreddy <rohan.katreddy@gmail.com>
2024-06-27 00:26:09 -0300

    Fixed search results bug (pushing other elements down)

--- scripts/search-bar.js
+++ scripts/search-bar.js
@@ -1,30 +1,29 @@
-import { cities } from "./data/cities.js";
+import { cities } from './data/cities.js';
 
-const resultBox = document.querySelector(".js-result-box");
-const inputBox = document.querySelector(".js-input-box");
-
+const resultBox = document.querySelector('.js-result-box');
+const inputBox = document.querySelector('.js-input-box');
 
 export function initSearchBar() {
+  inputBox.addEventListener('keyup', () => {
+    let results = [];
+    let input = inputBox.value;
+    if (input.length) {
+      results = cities.filter((keyWord) => {
+        return keyWord.toLowerCase().includes(input.toLowerCase());
+      });
+    }
 
-    inputBox.addEventListener("keyup", () => {
-        let results = [];
-        let input = inputBox.value;
-        if (input.length) {
-            results = cities.filter((keyWord) => {
-                return keyWord.toLowerCase().includes(input.toLowerCase());
-            });
-        }
-
-        displayResults(results);
-    });
+    displayResults(results);
+  });
 }
 
-function displayResults(results, num=5) {
-    // displays only the first num results
-    // TODO: display however many generated, but limit number generated? 
-    let resultHTML = "";
-    for (let i=0;i<Math.min(num, results.length);i++) {
-        resultHTML += `<li>${results[i]}</li>\n`;
-    }
-    resultBox.innerHTML = `<ul>${resultHTML}</ul>`;
+function displayResults(results, num = 5) {
+  // displays only the first num results
+  // TODO: display however many generated, but limit number generated?
+  let resultHTML = '';
+  for (let i = 0; i < Math.min(num, results.length); i++) {
+    resultHTML += `<li>${results[i]}</li>\n`;
+  }
+  resultBox.style.display = results.length === 0 ? 'none' : 'block';
+  resultBox.innerHTML = `<ul>${resultHTML}</ul>`;
 }
